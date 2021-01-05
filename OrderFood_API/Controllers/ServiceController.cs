393a9506f3fed5948cf1877ce74164fbdb37d25e
Duc Toan <ductoan20102000@gmail.com>
2021-01-05 23:17:41 +0700

    Lay ds loại món + Lay ds món theo loại

---
+++ OrderFood_API/Controllers/ServiceController.cs
@@ -0,0 +1,68 @@
+﻿using OrderFood_API.Services;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OrderFood_API.Controllers
+{
+    public class ServiceController : ApiController
+    {
+        [Route("api/ServiceController/DemoAPI")]
+        [HttpGet]
+        public IHttpActionResult DemoAPI(string name)
+        {
+            return Ok("Xin chao " + name);
+        }
+
+        [Route("api/ServiceController/getKhachHang")]
+        [HttpGet]
+        public IHttpActionResult getKhachHang()
+        {
+            DataTable kq = Database.Read_Table("KHACHHANG");
+            if (kq != null && kq.Rows.Count > 0)
+                return Ok(kq);
+            else
+                return NotFound();
+        }
+
+        [Route("api/ServiceController/getLoaiMon")]
+        [HttpGet]
+        public IHttpActionResult getLoaiMon()
+        {
+            DataTable kq = Database.Read_Table("LOAIMON");
+            if (kq != null && kq.Rows.Count >= 0)
+                return Ok(kq);
+            else
+                return NotFound();
+        }
+
+        [Route("api/ServiceController/getMonAn")]
+        [HttpGet]
+        public IHttpActionResult getMonAn()
+        {
+            DataTable kq = Database.Read_Table("MONAN");
+            if (kq != null && kq.Rows.Count >= 0)
+                return Ok(kq);
+            else
+                return NotFound();
+        }
+
+        [Route("api/ServiceController/getMonAnTheoLoai")]
+        [HttpGet]
+        public IHttpActionResult getMonAnTheoLoai(int MaLM)
+        {
+            var param = new Dictionary<string, object>() {
+                { "MaLM", MaLM }
+            };
+            DataTable kq = Database.Read_Table_SP("SP_GetMonTheoLoai", param);
+            if (kq != null && kq.Rows.Count >= 0)
+                return Ok(kq);
+            else
+                return NotFound();
+        }
+    }
+}
---
+++ OrderFood_API/Services/Database.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace OrderFood_API.Services
+{
+    public class Database
+    {
+        public static DataTable Read_Table_SP(string StoredProcedureName, Dictionary<string, object> dic_param = null)
+        {
+            string SQLconnectionString = ConfigurationManager.ConnectionStrings["OrderFood_DB"].ConnectionString;
+            DataTable result = new DataTable("table1");
+            using (SqlConnection conn = new SqlConnection(SQLconnectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = conn.CreateCommand();
+                SqlTransaction transaction;
+                // Start a local transaction.
+                transaction = conn.BeginTransaction("SampleTransaction");
+                cmd.Connection = conn;
+                cmd.CommandText = StoredProcedureName;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Transaction = transaction;
+                if (dic_param != null)
+                {
+                    foreach (KeyValuePair<string, object> data in dic_param)
+                    {
+                        if (data.Value == null)
+                        {
+                            cmd.Parameters.AddWithValue("@" + data.Key, DBNull.Value);
+                        }
+                        else
+                        {
+                            cmd.Parameters.AddWithValue("@" + data.Key, data.Value);
+                        }
+                    }
+                }
+                try
+                {
+                    SqlDataAdapter da = new SqlDataAdapter();
+                    da.SelectCommand = cmd;
+                    da.Fill(result);
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                }
+            }
+            return result;
+        }
+        public static object Exec_Scala(string StoredProcedureName, Dictionary<string, object> dic_param = null)
+        {
+            string SQLconnectionString = ConfigurationManager.ConnectionStrings["Connectionstring"].ConnectionString;
+            object result = null;
+            using (SqlConnection conn = new SqlConnection(SQLconnectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = conn.CreateCommand();
+                SqlTransaction transaction;
+                // Start a local transaction.
+                transaction = conn.BeginTransaction("SampleTransaction");
+                cmd.Connection = conn;
+                cmd.CommandText = StoredProcedureName;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Transaction = transaction;
+                if (dic_param != null)
+                {
+                    foreach (KeyValuePair<string, object> data in dic_param)
+                    {
+                        if (data.Value == null)
+                        {
+                            cmd.Parameters.AddWithValue("@" + data.Key, DBNull.Value);
+                        }
+                        else
+                        {
+                            cmd.Parameters.AddWithValue("@" + data.Key, data.Value);
+                        }
+                    }
+                }
+                try
+                {
+                    result = cmd.ExecuteScalar();
+                    // Attempt to commit the transaction.
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    result = null;
+                }
+            }
+            return result;
+        }
+        public static object Exec_Command(string StoredProcedureName, Dictionary<string, object> dic_param = null)
+        {
+            string SQLconnectionString = ConfigurationManager.ConnectionStrings["Connectionstring"].ConnectionString;
+            object result = null;
+            using (SqlConnection conn = new SqlConnection(SQLconnectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = conn.CreateCommand();
+                SqlTransaction transaction;
+                // Start a local transaction.
+                transaction = conn.BeginTransaction("SampleTransaction");
+                cmd.Connection = conn;
+                cmd.CommandText = StoredProcedureName;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Transaction = transaction;
+                if (dic_param != null)
+                {
+                    foreach (KeyValuePair<string, object> data in dic_param)
+                    {
+                        if (data.Value == null)
+                        {
+                            cmd.Parameters.AddWithValue("@" + data.Key, DBNull.Value);
+                        }
+                        else
+                        {
+                            cmd.Parameters.AddWithValue("@" + data.Key, data.Value);
+                        }
+                    }
+                }
+                cmd.Parameters.Add("@CurrentID", SqlDbType.Int).Direction = ParameterDirection.Output;
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    result = cmd.Parameters["@CurrentID"].Value;
+                    // Attempt to commit the transaction.
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    result = null;
+                }
+
+            }
+            return result;
+        }
+        public static DataTable Read_Table(string TableName)
+        {
+            string SQLconnectionString = ConfigurationManager.ConnectionStrings["OrderFood_DB"].ConnectionString;
+            DataTable result = new DataTable("table1");
+
+            using (SqlConnection conn = new SqlConnection(SQLconnectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = conn.CreateCommand();
+                SqlTransaction transaction;
+                // Start a local transaction.
+                transaction = conn.BeginTransaction("SampleTransaction");
+                cmd.Connection = conn;
+                cmd.CommandText = "select * from " + TableName;
+                cmd.CommandType = CommandType.Text;
+                cmd.Transaction = transaction;
+                try
+                {
+                    SqlDataAdapter da = new SqlDataAdapter();
+                    da.SelectCommand = cmd;
+                    da.Fill(result);
+                    //transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                }
+            }
+
+            return result;
+        }
+    }
+}
